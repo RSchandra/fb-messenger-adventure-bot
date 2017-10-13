7bdb1ae4fcb1fd6a2feb1a5a6d0f1c5fd7c186e2
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2017-10-13 08:14:14 -0400

    updated .gitignore for push

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = module.exports = {
 	FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
-	VERIFY_TOKEN: 'verify token here',
-	PROFILE_TOKEN: 'page access token here',
+	VERIFY_TOKEN: 'Secret6503',
+	PROFILE_TOKEN: 'EAAHlPpZBD4WUBAGpI1pywwYPR2BrsSTBFI8IJQyjLaZAbnbeF2ApGttj6t6FqLRnC9WVS3ZCyd45mfaVFz2xsMliOyEpSGEnX20tglCvAyccZB6tDutFOTZBDc0SBIKdAkfqiAZCfs2pE8Ng50FfiWaSSMQVMqkt1ZA7DdTOFFPfUGCpLS0eM5z91y9RsC9VBkZD',
 	PORT: '3000'
 };
